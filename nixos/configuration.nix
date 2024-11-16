4eee2f6d5ac9b02a13d79555bd0f7ea0e9cc1307
Daniel Ramirez <coffee-dan@protonmail.com>
2024-11-16 14:29:50 -0600

    update: mise support on nixos and starship config
    Mise installs precomplied binaries that rely on
    dynamically linked libraries which NixOS can only
    provide if ld is enabled.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,6 +87,12 @@
     ];
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
